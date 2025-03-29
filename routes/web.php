22badcdef41f870fdcf885ed08c97a6ccfeaad8a
jcala03 <jcala2@cuc.edu.co>
2025-03-29 02:35:32 -0500

    renombrando controlador, anadiedo logica al mismo y configurando rutas

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\InteresesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [InteresesController::class, 'index'])->name('intereses.index');
+Route::post('/calcular', [InteresesController::class, 'calcular'])->name('calcular.intereses');
